b41939cd506392f8d5f19fa247f2b6baacb506ff
Drew Wilken <wilk0001@stthomas.edu>
2018-03-02 17:42:19 -0600

    Got the toeknizing to work finally

--- Assembler.c
+++ Assembler.c
@@ -3,41 +3,27 @@
 //  Assembler-Simulator-XP
 //
 //  Created by Drew Wilken on 3/1/18.
-//  Copyright © 2018 Drew Wilken. All rights reserved.
+//  Copyright © 2018 Drew Wilken and Nathan Taylor. All rights reserved.
 //
 
-/*
- * TODO: since strtok already breaks up the string and puts null characters in between each one, then we can just use that
- * knowledge to take the addresses of each piece and then just add it to the main array.
- */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 char **split(char s[]);
+int countTokens(char* s);
 
 char ***instructions;
 
 int main() {
     
-    char s[] = "lw 1 2 0 yo hello ";
+    char s[] = "Push: lw 1  2 3 #hello man!";
+    int tokinstr = countTokens(s);
     char **instr = split(s);
     
     int i = 0;
-    int j = 0;
-    /*
-     char *test;
-     char str[] = "hellooooooooo";
-     printf("size of str: %i\n", sizeof(str));
-     test = malloc(strlen(str) * sizeof(char));
-     printf("size of test: %i\n", sizeof(test));
-     test = "hello";
-     
-     printf("%s\n", test);
-     
-     printf("%s\n", instr[2]);
-     */
-    for(i = 0; i<10; i++) {
+    
+    for(i = 0; i<tokinstr; i++) {
         printf("%s\n", instr[i]);
     }
     
@@ -59,33 +45,26 @@ int main() {
 
 
 
-//
-// Function to split the string into tokens
-// in a string array
-//
-// uses strtok() to break it into the tokens
-//
+/*
+ * Splits a string into tokens and returns a pointer to a str array
+ */
 char **split(char s[]) {
     char *token;
     int i = 0;
-    int count = 0;
+    int count = countTokens(s);
+    
+    char **instr = malloc((count+1) * sizeof(char*));
     
     //get a count of tokens so we can allocate memory for them
     token = strtok (s," ");
     while (token != NULL)
     {
-        count++;
+        //since token reference keeps changing and is static, use strdup instead.
+        instr[i] = strdup(token);
         token = strtok (NULL, " ");
+        
+        i++;
     }
-    printf("Count: %i\n", count);
-    //allocate memory for the str array so that it can be returned and not
-    //just a reference to the main memory stack
-    //char **instr = (char**)malloc(count * sizeof(char*));
-    char **instr = malloc((count+1) * sizeof(char*));
-    //char **instr = malloc(200);
-    
-    printf("instr size: %lu\n", sizeof(instr));
-    
     
     //replace all tabs with spaces
     while(s[i] != '\0') {
@@ -94,17 +73,24 @@ char **split(char s[]) {
         }
         i++;
     }
-    i=0;
-    token = strtok (s," ");
-    
-    //go through the string and while it isn't null, add the token to the array
-    while (token != NULL)
-    {
-        //allocate the memory needed for the size of the str
-        instr[i] = malloc((strlen(token)+1) * sizeof(char));
-        strcpy(instr[i++],token);
-        printf("%s\n", instr[i-1]);
-        token = strtok (NULL, " ");
-    }
     return instr;
 }
+
+/*
+ * Returns the amount of tokens in the str
+ */
+int countTokens(char* s) {
+    int i = 0;
+    int count = 0;
+    char lastChar = ' ';
+    
+    while(s[i] != '\0'){
+        if(lastChar == ' ' && s[i] != ' ') {
+            count++;
+        }
+        lastChar = s[i];
+        i++;
+    }
+    return count;
+}
+
